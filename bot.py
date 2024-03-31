fc9410968aa6ee49ca092e74c3d2af41d8143f56
Steven Huang <111419241+Suttibun@users.noreply.github.com>
2024-04-01 05:40:36 +1100

    updated basic functonalitie
    starting study session

---
+++ bot.py
@@ -0,0 +1,48 @@
+from attr import dataclass
+from discord.ext import commands
+import discord
+
+
+BOT_TOKEN = "MTIyMzkwMTkyMDY1ODE5ODU5OQ.GQ85r2.ZqQRdJGI6y-nzG-yY4fxSfGSrcqH8Dw9ExHzfI"
+CHANNEL_ID = 1223904426553507880
+
+
+@dataclass 
+class Session:
+    is_active: bool = False
+    start_time: int = 0
+
+
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+session = Session()
+
+@bot.event
+async def on_ready():
+    print("Hello! Study bot is ready!")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("Hello! Study bot is ready!")
+    
+@bot.command()
+async def start(ctx):
+    if session.is_active:
+        await ctx.send("A study session is already active")
+        
+    session.is_active = True
+    session.start_time = ctx.message.created_at.timestamp()
+    await ctx.send(f"Study session has started at {ctx.message.created_at}")
+
+@bot.command()
+async def end(ctx):
+    if not session.is_active:
+        await ctx.send("No session is active")
+        return
+        
+    session.is_active = False
+    end_time = ctx.message.created_at.timestamp()
+    duration = end_time - session.start_time
+    await ctx.send(f"Study ended after {duration} seconds")
+    
+    
+
+bot.run(BOT_TOKEN)
